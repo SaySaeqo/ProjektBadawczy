7663f397e22d0d5ccd2c36c055bfb09af9105df3
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2022-03-14 19:02:26 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,8 +1,8 @@
-def polynomial(x,y):
-  return pow(x,4)+pow(y,4)-0.62*pow(x,2)-0.62*pow(y,2)
+def polynomial(args):
+  return pow(args[0],4)+pow(args[1],4)-0.62*pow(args[0],2)-0.62*pow(args[1],2)
 
-def rosenbrock(x,y):
-	return 100*pow(y-pow(x,2),2)+pow(1-x,2)
+def rosenbrock(args):
+	return 100*pow(args[1]-pow(args[0],2),2)+pow(1-args[0],2)
 
 def get_function(n):
 	if n==1:
