d355df9cd0395a32e733bf23e7627fdf096ab71a
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2022-03-16 22:16:45 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from Functions import get_function
 from Genetic_Algorithm import genetic_func
+from Constants import *
 from Gradient_Algorithm import gradient_func
 
-
+#przyklad wywołania genetycznego
+#print("wynik",genetic_func(get_function(1),2,[[-10,10],[-10,10]],MIN,PROBE_NUMBER))
