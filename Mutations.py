661affbbb96b58caf290388e96e7c332ea4cb082
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2022-03-21 06:56:28 +0100

    Add files via upload

--- Mutations.py
+++ Mutations.py
@@ -16,12 +16,16 @@ def mutate_new_random(generation,domain_list,arg_num):
 			org.data=random_data(domain_list,arg_num)
 
 
-def mutate_add_little(generation,domain_list,arg_num):
+def mutate_change_little(generation,domain_list,arg_num):
 	mutated=[]
 	for org in generation:
-		if random.randint(0,100)>95 :
-			for i in range(len(org.data)):
-				org.data[i]+=0.00001
+		for i in range(len(org.data)):
+			if random.randint(0,100)>95 :
+				delta=0.01
+				if random.randint(0,1)==1:
+					org.data[i]+=delta
+				else:
+					org.data[i]-=delta
 
 
 def mutate_gradient_wise(generation,domain_list,arg_num):
