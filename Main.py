a6cbdad3b54701220a8b17ff8ae6a8832bad72fd
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2022-03-25 19:58:23 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,3 +5,7 @@ from Constants import *
 
 #przyklad wywołania genetycznego
 print("wynik",genetic_func_mean_random(get_function(1),2,[[-10,10],[-10,10]],MIN,PROBE_NUMBER))
+
+
+def funnckaj:
+  pass
