5b63f159f30ef8aff38d8d3ad765cc2bc5f5a351
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2022-03-25 20:13:59 +0100

    Add files via upload
    get function do zmodyfikowania 
    +
    dodanie klasy function

--- Main.py
+++ Main.py
@@ -4,7 +4,12 @@ from Constants import *
 from Gradient_Algorithm import *
 
 #przyklad wywołania genetycznego
-#print("wynik",genetic_func_mean_change(get_function(2),2,[[-10,10],[-10,10]],MIN,PROBE_NUMBER))
+
+
+func=get_function(2)
+
+#nie działa, bo get function nie zwraca dziedziny!
+#print("wynik",genetic_func_mean_change(func,2,domain,MIN,PROBE_NUMBER))
 
 #print("wynik",genetic_func_mean_gradient_change_litle(get_function(1),2,[[-10,10],[-10,10],[-10,10]],MIN,PROBE_NUMBER))
 
